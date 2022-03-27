f340fb5c0299db527dacd964bcf0c9303ead659a
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2022-03-28 00:02:29 +0200

    Update EventHandler.cs

--- MegaUtils/EventHandler.cs
+++ MegaUtils/EventHandler.cs
@@ -1,15 +1,30 @@
-using Exiled.API.Features;
-using Exiled;
-using System.Threading;
 using System;
+using System.Collections.Generic;
+using Exiled.API.Features;
+using MEC;
 using UnityEngine;
+using Exiled.Events.EventArgs;
 
 namespace MegaUtils
 {
-	partial class EventHandler
+	public class EventHandler
 	{
+		public CoroutineHandle tpsCoroutine;
+		public IEnumerator<float> TpsCoroutine()
+                {
+                    for (; ; )
+                    {
+                         Log.Info((int)Math.Round(1f / Time.smoothDeltaTime));
+                         yield return Timing.WaitForSeconds(MegaUtils.instance.Config.TpsCheckDelay);
+                    }
+                }
 		public void OnWaitingForPlayers()
-		{
-		}
+                {
+                     tpsCoroutine = Timing.RunCoroutine(TpsCoroutine());
+                }
+		public void OnRoundEnded(RoundEndedEventArgs ev)
+                {
+                     Timing.KillCoroutines(tpsCoroutine);
+                }
 	}
 }
